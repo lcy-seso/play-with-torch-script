dc39445e4c1121d1e8d2b342060424d61eaa3d2e
lcy-seso <lcy.seso@gmail.com>
2020-04-21 10:10:27 +0800

    reorganize files.

--- parser_test/srcs/stacked_lstm.c
+++ parser_test/srcs/stacked_lstm.c
@@ -1,11 +1,11 @@
 void StackLSTM(int N, int M, int K) {
   int a[N][M];
   int b[N][K];
-  int c[N][M][K]; // output array
+  int c[N][M][K];  // output array
 #pragma scop
-  for (int i = 0; i < N; ++i) {     // batch size
-    for (int j = 0; j < M; ++j) {   // sequence length
-      for (int k = 0; k < K; ++k) { // depth
+  for (int i = 0; i < N; ++i) {      // batch size
+    for (int j = 0; j < M; ++j) {    // sequence length
+      for (int k = 0; k < K; ++k) {  // depth
         if (k == 0) {
           if (j == 0) {
             c[i][j][k] = a[i][j] + b[i][k];
